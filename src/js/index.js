b0cb6cece0bec128d67f4f5f365d97b3a8a16d7a
Haiyang Hong <hong961127@gmail.com>
2020-11-22 17:13:32 -0800

    Use mouseover and mouse out and settimtout to implement tooltip functionaliteis

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ const tooltips = document.querySelectorAll(".tooltips .tooltip");
 const section = document.querySelector(".section");
 const container = document.querySelector(".container");
 
-function contentPosition() {
+function positionToolitp() {
   tooltips.forEach((tooltip) => {
     const pin = tooltip.querySelector(".tooltip__pin");
     const content = tooltip.querySelector(".tooltip__content");
@@ -23,5 +23,34 @@ function contentPosition() {
   });
 }
 
-contentPosition();
-["load", "resize"].forEach((evt) => window.addEventListener(evt, contentPosition));
+positionToolitp();
+["load", "resize"].forEach((evt) => window.addEventListener(evt, positionToolitp));
+
+let tooltipTimeoutId;
+
+// Add listeners to tooltips
+tooltips.forEach((tooltip) => {
+  const pin = tooltip.querySelector(".tooltip__pin");
+  const content = tooltip.querySelector(".tooltip__content");
+
+  // Mouse moves over on pin or tooltip
+  [pin, content].forEach((el) =>
+    el.addEventListener("mouseover", function () {
+      // If tooltip has a close timeout, clears it
+      if (tooltipTimeoutId) clearTimeout(tooltipTimeoutId);
+
+      // Open the tooltip
+      content.classList.add("tooltip__content--active");
+    })
+  );
+
+  // Mouse moves out on pin or tooltip
+  [pin, content].forEach((el) =>
+    el.addEventListener("mouseout", function () {
+      // Close tooltip in 1 second
+      tooltipTimeoutId = setTimeout(() => {
+        content.classList.remove("tooltip__content--active");
+      }, 1000);
+    })
+  );
+});
