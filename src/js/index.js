b2ded6a3e2b5d1a30f4a5f313a24589382749798
Haiyang Hong <hong961127@gmail.com>
2020-11-22 16:54:14 -0800

    Use element offset width to set and update tooltip position

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,26 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 import "../sass/main.scss";
+
+// DOM Elements
+const tooltips = document.querySelectorAll(".tooltips .tooltip");
+const section = document.querySelector(".section");
+const container = document.querySelector(".container");
+
+function contentPosition() {
+  tooltips.forEach((tooltip) => {
+    const pin = tooltip.querySelector(".tooltip__pin");
+    const content = tooltip.querySelector(".tooltip__content");
+    const arrow = tooltip.querySelector(".tooltip__content__arrow");
+
+    // pin.getBoundingClientRect().left is relative to viewport
+    // pin.offsetLeft is relative to it's parent
+
+    content.style.left = `${pin.offsetLeft - content.offsetWidth / 2}px`;
+    content.style.top = `${pin.offsetTop + 40}px`;
+    arrow.style.left = `${pin.offsetLeft - content.offsetLeft + pin.offsetWidth / 2}px`;
+  });
+}
+
+contentPosition();
+["load", "resize"].forEach((evt) => window.addEventListener(evt, contentPosition));
